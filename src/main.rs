473c57616f828b31dc83fc1a872f9c12760cc90c
ttlekich <ttlekich@gmail.com>
2023-03-26 18:49:33 -0500

    using Vec<Vec<T>> and fold...

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ pub struct Opts {
     population_size: u32,
 }
 
-fn initialize<T>(genotype: &dyn Fn() -> T, opts: Opts) {
+fn initialize<T>(genotype: &dyn Fn() -> Vec<T>, opts: Opts) {
     let population_size = opts.population_size;
 
     for _ in 0..population_size {
@@ -19,25 +19,26 @@ fn initialize<T>(genotype: &dyn Fn() -> T, opts: Opts) {
     }
 }
 
-fn evaluate<T>(mut population: Vec<T>, fitness_function: &dyn Fn(&T) -> u32, opts: Opts) {
+fn evaluate<T>(mut population: Vec<Vec<T>>, fitness_function: &dyn Fn(&Vec<T>) -> u32, opts: Opts) {
     population.sort_by(|a, b| fitness_function(a).cmp(&fitness_function(b)))
 }
 
-fn crossover<T>(mut population: Vec<T>, opts: Opts)
+fn crossover<T>(mut population: Vec<Vec<T>>, opts: Opts) -> Vec<Vec<T>>
 where
     T: Clone + ExactSizeIterator,
 {
-    let a = population
+    population
         .into_iter()
         .tuple_windows()
-        .reduce(|acc, (p1, p2)| {
+        .fold(&mut Vec::new(), |acc: Vec<Vec<T>>, (p1, p2)| {
             let crossover_point = rand::thread_rng().gen_range(0..p1.len());
-            (head_1, tail_1) = p1.group_by(|key| key < crossover_point);
-            // (head_2, tail_2) = p2.split_off(crossover_point);
-            // (child_1, child_2) = (head_1 + tail_2, head_2 + tail_1);
-            // acc + child_1 + child_2
-            acc
-        });
+            let (head_1, tail_1) = p1.split_at(crossover_point);
+            let (head_2, tail_2) = p2.split_at(crossover_point);
+            let mut child1 = head_1.to_vec();
+            let mut child2 = head_2.to_vec();
+            child1.append(&mut tail_2.to_vec());
+            child2.append(&mut tail_1.to_vec());
 
-    return;
+            acc
+        })
 }
